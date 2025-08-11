b420e6f3c3a50b4d3d4be701eebf88cc8238e25c
daniel4191 <daniell8282@naver.com>
2025-08-11 22:25:45 +0900

    상세페이지, 댓글 url 포함 추가

--- blog/views.py
+++ blog/views.py
@@ -10,5 +10,15 @@ def post_list(request):
     }
     return render(request, "post_list.html", context)
 
-def post_detail(request):
-    return render(request, "post_detail.html")
+# post_id를 인자로 받는것이 첫번째
+def post_detail(request, post_id):
+    
+    # post_id인자로 받아온 id 값에 해당하는 포스트자체를 가져온다.
+    # 표시는 해당 포스트의 제목만 표시한다. (가져오는 것은 해당 포스트의 모든 것이라, context로 html에 인자 넘겨주면
+    # models.py에 설정해두었던 해당 모델의 ORM을 모두 사용 가능함)
+    post = Post.objects.get(id=post_id)    
+    print(post)
+    context = {
+        "post": post
+    }
+    return render(request, "post_detail.html", context)
